d63f62ff7b27dfaf08b40723bbf1cab6672e96b7
Anton Omelchuk <kino_kg@ukr.net>
2026-07-10 19:12:26 +0300

    temp off settings vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import netlify from "@netlify/vite-plugin";
-import netlifyReactRouter from "@netlify/vite-plugin-react-router";
-import { reactRouter } from "@react-router/dev/vite";
+// import netlify from "@netlify/vite-plugin";
+// import netlifyReactRouter from "@netlify/vite-plugin-react-router";
+// import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -9,11 +9,11 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    reactRouter({
-      ssr: false,
-    }),
-    netlifyReactRouter(),
-    netlify(),
+    // reactRouter({
+    //   ssr: false,
+    // }),
+    // netlifyReactRouter(),
+    // netlify(),
   ],
   base: "/",
 });
