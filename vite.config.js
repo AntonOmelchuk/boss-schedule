f1dd1bb6d78a4e825396612d9eaf4b4b858cdb91
Anton Omelchuk <kino_kg@ukr.net>
2026-07-10 19:21:15 +0300

    remove libs

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
-// import netlify from "@netlify/vite-plugin";
-import netlifyReactRouter from "@netlify/vite-plugin-react-router";
-// import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    // reactRouter({
-    //   ssr: false,
-    // }),
-    netlifyReactRouter(),
-    // netlify(),
-  ],
+  plugins: [react(), tailwindcss()],
   base: "/",
 });
