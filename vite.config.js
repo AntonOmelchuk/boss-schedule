68e0614ec7bfe7cc98a7e31fbcbb9787bf2d421b
Anton Omelchuk <kino_kg@ukr.net>
2026-07-10 18:43:14 +0300

    change config for routes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
+import netlify from "@netlify/vite-plugin";
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
+import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "./",
+  plugins: [
+    react(),
+    tailwindcss(),
+    reactRouter(),
+    netlifyReactRouter(),
+    netlify(),
+  ],
+  base: "/",
 });
