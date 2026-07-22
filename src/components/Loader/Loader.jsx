4eff6d577b9b33918563ba4709ef0dfe398ce196
Anton Omelchuk <kino_kg@ukr.net>
2026-07-22 21:13:02 +0300

    hot fix

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,7 +2,7 @@ const Loader = ({ title }) => {
   return (
     <div className="flex justify-center items-center min-h-screen text-slate-400">
       <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-indigo-500"></div>
-      <span className="ml-3 text-lg font-medium">{title}</span>
+      <span className="ml-3 text-3xl font-medium">{title}</span>
     </div>
   );
 };
