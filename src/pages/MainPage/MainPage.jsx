8a9f5014c6a646d61b8bbdfaf9b14c5fd6c594a1
Anton Omelchuk <kino_kg@ukr.net>
2026-07-28 18:20:31 +0300

    clear passed alerts

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
+
+import useAppStore from "../../store/useAppStore";
 import AllEvents from "./components/AllEvents/AllEvents";
 import MainBlock from "./components/MainBlock/MainBlock";
 
 const MainPage = () => {
+  const eventsData = useAppStore((state) => state.eventsData);
+  const cleanExpiredAlerts = useAppStore((state) => state.cleanExpiredAlerts);
+
+  useEffect(() => {
+    if (eventsData?.length) {
+      cleanExpiredAlerts(eventsData);
+    }
+  }, [eventsData]);
   return (
     <div className="max-w-4xl mx-auto">
       <MainBlock />
