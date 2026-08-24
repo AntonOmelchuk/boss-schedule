267ace1677cbe0384840cbbd08680a3a080d52a1
Anton Omelchuk <kino_kg@ukr.net>
2026-08-24 13:06:39 +0300

    fix backend url

--- src/store/useDashboardStore.js
+++ src/store/useDashboardStore.js
@@ -1,7 +1,6 @@
 import { create } from "zustand";
 
-// const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 export const useDashboardStore = create((set) => ({
   data: null,
